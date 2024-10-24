b7a6a5b6f9f36844b08b4d5b086b08645b3b5859
rzemla <rzemla@gmail.com>
2024-10-24 00:41:15 -0400

    loads EKG traces from PDF input data with multiprocessing

--- tools/extraction_tools.py
+++ tools/extraction_tools.py
@@ -37,6 +37,91 @@ from utils.extract_utils.extract_utils import (adjust_leads_baseline,
                                                scale_values_based_on_eich_peak)
 
 
+
+
+
+#extract short lead traces from input PDF path
+def extract_traces(filename):
+    # reference value for the calibration jag
+    cal_ref = 1000
+
+    # extracted height for the calibration jag in PDF (extract pixel height from PDF)
+    # orginal provided measurement from codebase 28.34800000000001 (height that is read in via pdf reader)
+    # extracted by measuring pixel height in Illustrator (28.35)
+    cal_height = 1000  # height that is read in with pdf reader with our EKG pdfs
+
+
+    # factor for scaling (effectively gamma = 1 based on read in structure from GE EKGs)
+    gamma = cal_ref / cal_height
+
+    # name of the leads
+    lead_names_short = [
+        "I",
+        "II",
+        "III",
+        "aVR",
+        "aVL",
+        "aVF",
+        "V1",
+        "V2",
+        "V3",
+        "V4",
+        "V5",
+        "V6",
+    ]
+    lead_names_long = ["V1", "II", "V5"]
+
+
+    # Extract raw PDF data from source file
+    pdf_object = extract_PDF_raw(filename)
+
+
+    # define dict with index ranges for graphics object corresponding to leads/cal marker
+    range_list = {
+        # range of lead extraction (short leads)
+        "start_idx": 412,
+        "end_idx": 423,
+        # range of lead extraction (long leads)
+        "start_idx_long": 427,
+        "end_idx_long": 429,
+        # calibration marker index
+        "cal_idx": 424,
+    }
+
+    # Extract numeric x,y points for each lead
+    lead_numeric = extract_leads_numeric(range_list, pdf_object)
+
+    # Shift traces to common origin
+    shift_leads = list(map(move_trace_to_origin, lead_numeric))
+
+    # Scale leads by gamma factor (gamma = 1 for current EKG PDFs)
+    scale_shift_leads = [scale_values_using_cal_mark(l, gamma) for l in shift_leads]
+
+    # convert traces to by linear interpolation to timepoints along sampling frequency (500 Hz)
+    # for 2.5s strips (500Hz acquisition freq for 2.5s)
+    sample_points = 1250
+    leads_upsampled = [
+        create_measurement_points(l, sample_points) for l in scale_shift_leads[0:12]
+    ]
+
+    # for 10s strips (500Hz acquisition frq for 10s)
+    sample_points = 5000
+    leads_upsampled.extend(
+        [create_measurement_points(l, sample_points) for l in scale_shift_leads[13:16]]
+    )
+
+    # Convert short lead list to dataframe
+    df_leads_short = pd.DataFrame(
+        perform_shape_switch(leads_upsampled[0:12]), columns=lead_names_short
+    )
+
+    # Adjust baseline position of each lead
+    df_leads_short = adjust_leads_baseline(df_leads_short)
+
+    return df_leads_short
+
+
+
 def extract_PDF_raw(filename):
     reader = PyPDF2.PdfReader(open(filename, 'rb'))
     pg1 = reader.pages[0].get_contents()._data
